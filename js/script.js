81965ebce94c19d6718ca708c6e051a316c1a522
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-06-10 21:43:11 +0600

    book added fuction created(front-end)

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let form = document.querySelector('#book-form');
 
 // book class
 
-class book{
+class Book{
     constructor(title , author, isbn){
         this.title = title;
         this.author = author;
@@ -20,12 +20,44 @@ class book{
     }
 }
 
+//UI class
+class UI{
+    constructor(){
+
+    }
+    addToBookList(book){
+        let list = document.querySelector('#book-list');
+        let row = document.createElement('tr');
+        row.innerHTML = `<td>${book.title}</td>
+                        <td>${book.author}</td>
+                        <td>${book.isbn}</td>
+                        <td><a href = "#" class = "delete">X</a></td>`
+
+        list.appendChild(row);
+    }
+    clearFields(book){
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+
+    }
+}
+
 
 //define function
 function newBook(e){
-    console.log('hello');
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let isbn = document.querySelector('#isbn').value;
+    //book object created
+    let book = new Book(title , author , isbn);
+    let ui = new UI();
+    ui.addToBookList(book);
+    ui.clearFields(book);
+
     e.preventDefault();
 }
+
 
 
 
